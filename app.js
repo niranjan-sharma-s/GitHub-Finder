bb9dd00f3c489e30c346aa8c9da1b799fe437981
niranjan-sharma-s <niranjansharma1992@gmail.com>
2021-05-07 12:15:23 +0530

    added comment

--- app.js
+++ app.js
@@ -5,30 +5,28 @@ const github = new GitHub();
 const ui = new UI();
 
 //get user input
-const searchUser = document.getElementById('searchUser');
+const searchUser = document.getElementById("searchUser");
 
 //Add Event listenner
-searchUser.addEventListener('keyup', (e) => {
-
+searchUser.addEventListener("keyup", (e) => {
   const userText = e.target.value;
 
-  if (userText !== ''){
+  if (userText !== "") {
     //make http call to get data
-    github.getUser(userText)
-      .then(data => {
-        if (data.profile.message ==='Not Found') {
-          //Alert user not found ui.js
-          ui.showAlert('user not found','alert alert-danger');
-
-        } else {
-          //Display results ui.js
-                ui.showProfile(data.profile);
-                ui.showRepos(data.repos);
-        }
-      })
+    github.getUser(userText).then((data) => {
+      if (data.profile.message === "Not Found") {
+        //Alert user not found ui.js
+        ui.showAlert("user not found", "alert alert-danger");
+      } else {
+        //Display results ui.js
+        ui.showProfile(data.profile);
+        ui.showRepos(data.repos);
+      }
+    });
   } else {
     //Clear profile ui.js
     ui.clearProfile();
-
   }
-})
+});
+
+console.log("comment");
